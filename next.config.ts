bd3fdfd0fe4d7c117d9b5d99627fb49a5776f5d3
spacewhale-zion <ar559993@gmail.com>
2025-10-22 19:30:36 +0530

    remove export from nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // generates static HTML
   images: {
     unoptimized: true, // disables image optimization (needed for GitHub Pages)
   },
